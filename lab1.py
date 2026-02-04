7ed3668238c3ffffeedac92cab045ec2afd59b37
tim-degroot <timdegroot345@gmail.com>
2026-02-04 10:02:35 +0100

    fixed mol calculations

--- lab1.py
+++ lab1.py
@@ -1,9 +1,9 @@
 from scipy.integrate import solve_ivp 
 from functools import partial
 
-Vmax = 5.5 # mmol L-1 min-1
-Km = 50 # mM
-Ki = 3.5 # mM
+# Vmax = 5.5 # mmol L-1 min-1
+# Km = 50 # mM
+# Ki = 3.5 # mM
 
 Mlactose = 342.3 # g mol-1
 milk_lactose_concentration = 25 # g L-1
@@ -32,8 +32,8 @@ safe_limit_concentration = 1 # g L-1
 def lactase_activity(t, S, Vmax, Km):
     return -(Vmax * S)/(Km + S)
 
-lactose_in_milk_mol = volume_milk * milk_lactose_concentration * Mlactose
-safe_limit_mol = volume_milk * safe_limit_concentration * Mlactose
+lactose_in_milk_mol = volume_milk * milk_lactose_concentration / Mlactose
+safe_limit_mol = volume_milk * safe_limit_concentration / Mlactose
 
 def lactose_safe(t, S, Vmax, Km):
     return S[0] - safe_limit_mol
@@ -42,5 +42,8 @@ lactose_safe.terminal = True
 lactose_safe.direction = -1
 
 print(lactose_in_milk_mol, safe_limit_mol)
-sol = solve_ivp(lactase_activity, [0, 10000], [lactose_in_milk_mol], args=(Vmax, Km), events=lactose_safe)
+sol = solve_ivp(lactase_activity, (0, 1000), [lactose_in_milk_mol], args=(Vmax, Km), events=lactose_safe)
+
 print(sol.y)
+print(sol.y_events)
+print(sol)

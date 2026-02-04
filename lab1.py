a6cf74cd9aabc4db6fc8fc1344284bc2a67004f9
tim-degroot <timdegroot345@gmail.com>
2026-02-04 10:23:11 +0100

    remove stuff

--- lab1.py
+++ lab1.py
@@ -11,24 +11,6 @@ Mlactose = 342.3 # g mol-1
 milk_lactose_concentration = 25 # g L-1
 volume_milk = 1000 # l
 
-"""
-First we want to convert all units to base units so mol, M (molar), g, liters, and minutes.
-Vmax: 5.5 * 10^-3 / 60 = 5.5e-3 # mol L-1 min-1
-Km: 50 * 10^-3 = 5.0e-2 # M
-Ki: 3.5 * 10^-3 = 3.5e-3 # M
-"""
-
-Vmax = 5.5e-3 # mol L-1 min-1
-Km = 5.0e-2 # M
-Ki = 3.5e-3 # M
-
-"""
-v = vmax*S / Km + S = [mol L-1 min-1][mol] / [mol L-1] + [mol L-1] = [mol^2 L-1 min-1]/[2 (mol L-1)]
-v = mol/min
-
-dS/dt = v = (vmax * S)/(Km + s)
-"""
-
 safe_limit_concentration = 1 # g L-1
 
 def lactase_activity(t, S, Vmax, Km):

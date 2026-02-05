558859d4405a9989cdad217b2ece148936f86907
tim-degroot <timdegroot345@gmail.com>
2026-02-05 16:37:51 +0100

    question1 - first version, seems to answer everything.

---
+++ assignment1/equations.md
@@ -0,0 +1,10 @@
+<!--- pandoc equations.md -o equations.pdf -V geometry:margin=1in --->
+# Assignment 1: Equations
+## Derivation of $K_{m2}$
+$$\begin{align}
+v &= \frac{V_{max}S_1S_2}{K_{m1}S_2+K_{m2}S_1+S_1S_2} \\
+v(K_{m1}S_2+K_{m2}S_1+S_1S_2) &= V_{max}S_1S_2 \\
+K_{m1}S_2+K_{m2}S_1+S_1S_2 &= \frac{V_{max}S_1S_2}{v} \\
+K_{m2}S_1 &= \frac{V_{max}S_1S_2}{v} - K_{m1}S_2 - S_1S_2 \\
+K_{m2} &= \frac{V_{max}S_2}{v} - \frac{K_{m1}S_2}{S_1} - S_2
+\end{align}$$
--- assignment1/main.py
+++ assignment1/main.py
@@ -5,10 +5,11 @@ from scipy.integrate import solve_ivp
 
 kinetics = pd.read_csv("Kinetics.csv")
 
+print('Question 1: The "Living" Ice')
 print('Analyze the data and determine which mechanism "Europase" follows, justify yourconclusions statistically.')
 
-print("|$S_2$|Slope ($K_m/V_\\text{max}$)|Y-Intercept ($1/V\\text{max}$)|X-Intercept ($-1/K_m$)|$R^2$|$V\\text{max}$|$K_m$|")
-print("|-----|---------------------------|------------------------------|----------------------|-----|--------------|-----|")
+print("|$S_2$|$K_m/V_\\text{max}$|$1/V\\text{max}$|$-1/K_{m1}$|$R^2$|$V\\text{max}$|$K_{m1}$|")
+print("|-----|-------------------|----------------|-----------|-----|--------------|-----|")
 for value in kinetics["S2"].unique():
     data = kinetics.loc[kinetics["S2"]==value]
 
@@ -49,9 +50,77 @@ plt.legend(title=r"$S_2$")
 plt.title("Two substrate kinetics - Type 2 - Ping-pong mechanism")
 plt.savefig("pingpong.jpg")
 # plt.show()
+plt.clf()
+
+print("\nFind Km2")
+S2 = 10000.0
+Vmax = 1.0000
+Km1 = 0.1000
+
+Km2 = []
+data = kinetics.loc[kinetics["S2"]==S2]
+for index, row in data.iterrows():
+    result = Vmax * S2 / row["Rate"] - Km1 * S2 / row["S1"] - S2
+    Km2.append(result)
+
+print(f"Km2 = {np.mean(Km2):.4f} +- {np.std(Km2):.3e}")
 
 print()
 
+# exit()
 print("To synthesize the bio-polymer, an industrial batch reactor will be loaded with the co-factor (S2) in excess and an initial concentration of 100 g/L of the mineral substrate (S1). Calculate how much time (in seconds or minutes) is required to deplete the substrate S1 to below 1 g/L. Assume the Molecular Weight of substrate S1 is 150 g/mol.")
 
-Vmax, Km = 1.0000, 0.1000 # use values for [S2] = 10000.0 since S2 is in excess
+Vmax, Km1 = 1.0000, 0.1000 # use values for [S2] = 10000.0 since S2 is in excess
+S2 = 10000.0
+Km2 = 0.1000
+S1_MW = 150 # g/mol
+S1_initial = 100 # g/L
+S1_safe = 1 # g/L
+
+S1_initial_mM = S1_initial / S1_MW * 1000
+S1_safe_mM = S1_safe / S1_MW * 1000
+
+def safe_limit(t, S, *args):
+    return S[0] - S1_safe_mM
+
+safe_limit.terminal = False
+safe_limit.direction = -1
+
+def zero(t, S, *args):
+    return S[0]
+
+zero.terminal = True
+zero.direction = -1
+
+def reaction_ODE(t, S, Vmax, Km):
+    """
+    dS/dt = v = -(Vmax * S)/(Km + S)
+    """
+
+    return -(Vmax * S*S2) / (Km1*S2 + Km2*S+S*S2)
+
+
+t_span = (0, 900)
+t_eval = np.linspace(t_span[0], t_span[1], t_span[1]*10)
+
+sol = solve_ivp(
+    reaction_ODE,
+    t_span,
+    [S1_initial_mM],
+    args=(Vmax, Km1),
+    events=[safe_limit, zero],
+    t_eval=t_eval,
+)
+
+
+print(f"It took {sol.t_events[0][0]:.2f} seconds ({sol.t_events[0][0]/60:.1f} minutes) to reach a concentration below {S1_safe} g/L.")
+plt.title(r"Concentration of $S_1$ over time")
+plt.plot(sol.t, sol.y[0])
+plt.ylabel(r"$S_1$ (mM)")
+plt.xlabel("Time (s)")
+plt.axhline(y=S1_safe_mM, linestyle="dotted", alpha=0.5, color="k")
+plt.plot(sol.t_events[0], sol.y_events[0][0], "x", color="k")
+
+plt.savefig("Concentration.jpg")
+# plt.show()
+plt.clf()

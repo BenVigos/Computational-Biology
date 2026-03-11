a0eb29967314cfcaee850db4b0b6b5f7e9324348
tim-degroot <timdegroot345@gmail.com>
2026-03-11 09:42:37 +0100

    remove simulation

--- assignment3/Simulation/Simulation/Simulation.py
+++
@@ -1,5 +0,0 @@
-from cc3d import CompuCellSetup
-from SimulationSteppables import SimulationSteppable
-
-CompuCellSetup.register_steppable(steppable=SimulationSteppable(frequency=1))
-CompuCellSetup.run()
